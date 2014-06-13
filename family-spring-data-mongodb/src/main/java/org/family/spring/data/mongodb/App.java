cd85393dc056ccb3536f09fb0de1b9fa07560aa1
wuziliang <wuziliang@dayima.com>
2014-06-13 17:25:26 +0800

    spring data mongodb

---
+++ family-spring-data-mongodb/src/main/java/org/family/spring/data/mongodb/App.java
@@ -0,0 +1,13 @@
+package org.family.spring.data.mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
